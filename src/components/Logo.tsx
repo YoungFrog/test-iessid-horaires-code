52618f6669825d275b267ff7173adf177f63dbee
Nicolas Richard <theonewiththeevillook@yahoo.fr>
2023-02-06 17:09:32 +0100

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-const logo = require('../../img/HE2B-Logo-ESI.png')
+const logo = require('../../img/HE2B-Logo-IESSID-300.webp')
 
 const Logo = (): JSX.Element => {
   return <img id="logo" src={logo} alt="logo ESI" className="img-fluid" />
